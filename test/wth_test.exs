4fe446c5f0cfe4cb855738c2426f235ab8218cb3
Eduardo Gurgel <eduardo@gurgel.me>
2014-03-22 05:42:55 +0000

    Update to Elixir 0.12.5

--- test/wth_test.exs
+++ test/wth_test.exs
@@ -4,22 +4,22 @@ defmodule WTH.WTHTest do
   import ExUnit.CaptureIO
 
   test "call WTH with an empty argument" do
-    assert capture_io(fn -> WTH.main([""]) end) =~ %r/Usage:/
+    assert capture_io(fn -> WTH.main([""]) end) =~ ~r/Usage:/
   end
 
   test "call WTH without an argument" do
-    assert capture_io(fn -> WTH.main([]) end) =~ %r/Usage:/
+    assert capture_io(fn -> WTH.main([]) end) =~ ~r/Usage:/
   end
 
   test "call WTH with one word argument" do
     use_cassette "urbandict_1_term" do
-      assert capture_io(fn -> WTH.main(["wth"]) end) =~ %r/What the Hell/
+      assert capture_io(fn -> WTH.main(["wth"]) end) =~ ~r/What the Hell/
     end
   end
 
   test "call WTH with two words argument" do
     use_cassette "urbandict_2_terms" do
-      assert capture_io(fn -> WTH.main(["banana", "boat"]) end) =~ %r/an all male gay cruise/
+      assert capture_io(fn -> WTH.main(["banana", "boat"]) end) =~ ~r/an all male gay cruise/
     end
   end
 end
