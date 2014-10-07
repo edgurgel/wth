067e2f80e64443d9487deccbe09e137dd87a22cf
Eduardo Gurgel <eduardo@gurgel.me>
2014-10-07 21:20:15 +1300

    Update to Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WTH.Mixfile do
     [ app: :wth,
       version: "0.0.5",
       name: "WTH",
-      elixir: "~> 0.14.2 or ~> 0.15.0",
+      elixir: "~> 0.15.0 or ~> 1.0",
       escript: [ main_module: WTH ],
       deps: deps ]
   end
@@ -16,8 +16,7 @@ defmodule WTH.Mixfile do
   end
 
   defp deps do
-    [ { :httpoison, "~> 0.3.0" },
-      { :hackney,   github: "benoitc/hackney", tag: "0.13.0" },
+    [ { :httpoison, "~> 0.4.0" },
       { :jsex, "~> 2.0" },
       { :meck, "0.8.2", github: "eproxus/meck", override: true, only: :test },
       { :exvcr, "~> 0.3.0", only: :test } ]
