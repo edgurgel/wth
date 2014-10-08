b866726f636f5960d41de0bb06ee87c3439e9c20
Eduardo Gurgel <eduardo@gurgel.me>
2014-10-08 21:12:56 +1300

    Use meck hex package

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule WTH.Mixfile do
   defp deps do
     [ { :httpoison, "~> 0.4.0" },
       { :jsex, "~> 2.0" },
-      { :meck, "0.8.2", github: "eproxus/meck", override: true, only: :test },
+      { :meck, "~> 0.8.2", only: :test },
       { :exvcr, "~> 0.3.0", only: :test } ]
   end
 end
